75839a5bdb8113e420662c705bf8793a737a86bb
Casperdev4 <allam.bilal91@gmail.com>
2025-01-28 11:56:09 +0100

    gg

--- traitement.php
+++ traitement.php
@@ -68,7 +68,7 @@ try {
     $mail->Port       = 465;
 
     $mail->setFrom('contact@webprime.fr', 'W.Nicolas');
-    $mail->addAddress('w.nicolascouverture@gmail.com');
+    $mail->addAddress('teddycouverture77@gmail.com');
     $mail->addAddress('webprime91@hotmail.com');
     $mail->CharSet = 'UTF-8';
     $mail->isHTML(true);
